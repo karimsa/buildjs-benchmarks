b6717d356138176afb33525e35e8280509400e66
Karim Alibhai <karim@alibhai.co>
2017-06-13 19:30:23 -0400

    Move babel outside of travis

---
+++ bench.dist.js
@@ -0,0 +1,160 @@
+'use strict';
+
+const { exec: _exec } = require('child_process');
+const { exists, readFile, writeFile } = require('mz/fs');
+const { mkdirSync: mkdir, createWriteStream, readFileSync, writeFileSync } = require('fs');
+const { sync: rf } = require('rimraf');
+const asar = require('asar');
+const Benchmkark = require('benchmark'
+
+/**
+ * Cleanup & create sandbox.
+ */
+);rf(`${__dirname}/build`);
+mkdir(`${__dirname}/build`
+
+/**
+ * Easily configurable to add tools.
+ */
+);const tools = ['gulp', 'grunt', 'fly', 'brunch'];
+
+/**
+ * Wraps up an async function into the
+ * deferred-style used by benchmarkjs.
+ */
+function defer(fn) {
+  return prom => {
+    fn().then(prom.resolve.bind(prom), err => {
+      err = err || new Error('Something went wrong.');
+      console.error(err.stack || err);
+      process.exit(-1);
+    });
+  };
+}
+
+/**
+ * Synchronous file copy.
+ */
+function cp(src, dist) {
+  writeFileSync(dist, readFileSync(src, 'utf8'));
+}
+
+/**
+ * async child_process.exec - different from the mz/
+ * implementation. this one gives me stderr separate
+ * from stdout.
+ */
+function exec(test, tool, args, logs) {
+  return new Promise((resolve, reject) => {
+    const child = _exec(`node_modules/.bin/${tool} ${args}`, {
+      cwd: `${__dirname}/${test}/${tool}`
+    });
+    const { stderr, stdout } = child;
+
+    stderr.pipe(process.stderr);
+    stdout.on('data', data => logs[tool].write(data));
+
+    child.on('err', reject);
+
+    child.on('exit', code => {
+      if (code !== 0) reject();else resolve();
+    });
+  });
+}
+
+/**
+ * Cache and load args for build commands.
+ */
+const args = {};
+
+async function getArgs(test, tool) {
+  const key = test + '-' + tool;
+
+  if (args.hasOwnProperty(key)) {
+    return args[key];
+  }
+
+  const argsFile = `${__dirname}/${test}/${tool}/args`;
+  return args[key] = (await exists(argsFile)) ? await readFile(argsFile) : '';
+}
+
+/**
+ * Runs single test.
+ */
+function run(test) {
+  return new Promise((resolve, reject) => {
+    mkdir(`${__dirname}/build/${test}`);
+
+    const suite = new Benchmkark.Suite();
+    const logs = {};
+
+    /**
+     * Add test runner for each tool & use streams
+     * for log management to decrease overhead.
+     */
+    for (let tool of tools) {
+      rf(`${__dirname}/${test}/${tool}/src`);
+
+      mkdir(`${__dirname}/${test}/${tool}/src`);
+      mkdir(`${__dirname}/${test}/${tool}/src/js`);
+      mkdir(`${__dirname}/${test}/${tool}/src/css`);
+
+      cp(`${__dirname}/node_modules/jquery/dist/jquery.js`, `${__dirname}/${test}/${tool}/src/js/jquery.js`);
+      cp(`${__dirname}/node_modules/bootstrap/dist/js/bootstrap.js`, `${__dirname}/${test}/${tool}/src/js/bootstrap.js`);
+      cp(`${__dirname}/node_modules/bootstrap/dist/css/bootstrap.css`, `${__dirname}/${test}/${tool}/src/css/bootstrap.css`);
+
+      logs[tool] = createWriteStream(`${__dirname}/build/${test}/build-${tool}.log`);
+
+      suite.add(tool, defer(async () => {
+        await exec(test, tool, (await getArgs(test, tool)), logs);
+      }), {
+        defer: true
+      });
+    }
+
+    /**
+     * Run the benchmarks.
+     */
+    console.log('%s:\n', test);
+
+    suite.on('cycle', evt => console.log(String(evt.target))).on('complete', function () {
+      console.log('');
+      console.log('Fastest is ' + this.filter('fastest').map('name')
+
+      /**
+       * Close all logs, since we're about to zip & end.
+       */
+      );for (let tool in logs) {
+        if (logs.hasOwnProperty(tool)) {
+          logs[tool].end();
+        }
+      }
+
+      /**
+       * Next test.
+       */
+      resolve();
+    }).run({ 'async': true });
+  });
+}
+
+/**
+ * Run individual tests.
+ */
+;(async () => {
+  const tests = ['simple'];
+
+  for (let test of tests) {
+    await run(test);
+  }
+})().then(() => {
+  /**
+   * Create log package for release.
+   */
+  asar.createPackage(`${__dirname}/build`, 'build.asar', () => {
+    // do nothing - node should exit
+  });
+}).catch(err => {
+  console.error(err.stack || err);
+  process.exit(-1);
+});
