21eddbfba034471e78612b2887b8f92269b9602d
Luke Edwards <luke.edwards05@gmail.com>
2017-06-27 00:28:48 -0700

    Cleanup flyfiles (#1)
    * rewrite flyfiles
    
    * use full param name

--- simple/fly/flyfile.js
+++ simple/fly/flyfile.js
@@ -1,13 +1,11 @@
-exports.js = function* () {
-  yield this.source([ 'src/js/*.js' ])
-            .target('dist/js')
-}
-
-exports.css = function* () {
-  yield this.source([ 'src/css/*.css' ])
-            .target('dist/css')
-}
-
-exports.default = function* () {
-  yield this.parallel(['js', 'css'])
+module.exports = {
+	*js(fly) {
+		yield fly.source('src/js/*.js').target('dist/js')
+	},
+	*css(fly) {
+		yield fly.source('src/css/*.css').target('dist/css')
+	},
+	*default(fly) {
+		yield fly.parallel(['js', 'css'])
+	}
 }
--- tons.of.big.files/fly/flyfile.js
+++ tons.of.big.files/fly/flyfile.js
@@ -1,4 +1,3 @@
-exports.default = function* () {
-  yield this.source([ 'src/js/*.js' ])
-            .target('dist/js')
+exports.default = function* (fly) {
+	yield fly.source('src/js/*.js').target('dist/js')
 }
