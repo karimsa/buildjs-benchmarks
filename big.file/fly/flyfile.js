b6a5cf8fe2a59b776fe87f37fda4579d81c79605
Luke Edwards <luke.edwards05@gmail.com>
2017-06-26 23:24:30 -0700

    rewrite flyfiles

--- big.file/fly/flyfile.js
+++ big.file/fly/flyfile.js
@@ -1,4 +1,3 @@
-exports.default = function* () {
-  yield this.source([ 'src/js/*.js' ])
-            .target('dist/js')
+exports.default = function* (f) {
+	yield f.source('src/js/*.js').target('dist/js')
 }
--- simple/fly/flyfile.js
+++ simple/fly/flyfile.js
@@ -1,13 +1,11 @@
-exports.js = function* () {
-  yield this.source([ 'src/js/*.js' ])
-            .target('dist/js')
-}
-
-exports.css = function* () {
-  yield this.source([ 'src/css/*.css' ])
-            .target('dist/css')
-}
-
-exports.default = function* () {
-  yield this.parallel(['js', 'css'])
+module.exports = {
+	*js(f) {
+		yield f.source('src/js/*.js').target('dist/js')
+	},
+	*css(f) {
+		yield f.source('src/css/*.css').target('dist/css')
+	},
+	*default(f) {
+		yield f.parallel(['js', 'css'])
+	}
 }
